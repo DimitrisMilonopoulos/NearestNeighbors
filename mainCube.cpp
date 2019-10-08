47a1f96036a3d37a88cf28913b4eb50de9f7af19
nasquik <nassiatourn@gmail.com>
2019-10-08 19:04:36 +0300

    read from command line

---
+++ mainCube.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unistd.h>
+#include <getopt.h>
+#include <string>
+
+using namespace std;
+
+// $./cube –d <input file> –q <query file> –k <int> -M <int> -probes <int> -ο <output file>
+
+int main(int argc, char* argv[]){
+
+    if(argc != 13){
+        cout << "Invalid Input!\n";
+        return -1;
+    }
+
+    string inputFile, queryFile, outputFile;
+    int k, M, probes;
+
+string optionBuffer, parameterBuffer;
+
+    for(int i = 1; i <= (argc-1); i+=2){
+
+        optionBuffer = string(argv[i]);
+        parameterBuffer = string(argv[i+1]);
+
+        if(optionBuffer == "-d")
+            inputFile = parameterBuffer;
+        else if(optionBuffer == "-q")
+            queryFile = parameterBuffer;
+        else if(optionBuffer == "-k")
+            k = stoi(parameterBuffer, nullptr, 10);
+        else if(optionBuffer == "-M")
+            M = stoi(parameterBuffer, nullptr, 10);
+        else if(optionBuffer == "-probes")
+            probes = stoi(parameterBuffer, nullptr, 10);
+        else if(optionBuffer == "-o")
+            outputFile = parameterBuffer;
+    }
+
+    cout << inputFile << " " << queryFile << " " << k << " " << M << " " << probes << " " << outputFile << endl;
+}
---
+++ mainLSH.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// $./lsh –d <input file> –q <query file> –k <int> -L <int> -ο <output file>
+
+int main(int argc, char* argv[]){
+
+    if(argc != 11){
+        cout << "Invalid Input!\n";
+        return -1;
+    }
+
+    string inputFile, queryFile, outputFile;
+    int k, L;
+
+string optionBuffer, parameterBuffer;
+
+    for(int i = 1; i <= (argc-1); i+=2){
+
+        optionBuffer = string(argv[i]);
+        parameterBuffer = string(argv[i+1]);
+
+        if(optionBuffer == "-d")
+            inputFile = parameterBuffer;
+        else if(optionBuffer == "-q")
+            queryFile = parameterBuffer;
+        else if(optionBuffer == "-k")
+            k = stoi(parameterBuffer, nullptr, 10);
+        else if(optionBuffer == "-L")
+            L = stoi(parameterBuffer, nullptr, 10);
+        else if(optionBuffer == "-o")
+            outputFile = parameterBuffer;
+    }
+
+    cout << inputFile << " " << queryFile << " " << k << " " << L << " " << outputFile << endl;

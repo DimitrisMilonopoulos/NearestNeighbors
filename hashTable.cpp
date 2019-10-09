83a6253a7430db711e46c84687e911ed2d8146b8
DimitrisMilonopoulos <dimitriosmilonopoulos@gmail.com>
2019-10-09 17:26:53 +0300

    Hash Table non final

--- hashTable.cpp
+++ hashTable.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
 #include "hashTable.hpp"
+
+using namespace std;
 
 unsigned int manhattanDist(class Item * item1, class Item* item2){
     unsigned int distance = 0;
@@ -11,4 +17,21 @@ unsigned int manhattanDist(class Item * item1, class Item* item2){
         distance += abs(coord_1[i]-coord_2[i]);
     }
     return distance;
+}
+
+HashTable::HashTable(unsigned int size){
+    bucketSize = size;
+    buckets = new vector<class Item *>[bucketSize];
+}
+
+HashTable::~HashTable(){
+    delete []buckets;
+}
+
+int HashTable::insertItem(class Item* item,unsigned int position){
+    if (position>=bucketSize){
+        cout << "Invalid hash table position"<<endl;
+        return 1;
+    }
+    buckets[position].push_back(item);
 }
--- hashTable.hpp
+++ hashTable.hpp
@@ -3,7 +3,22 @@
 
 #include "fileReading.hpp"
 #include <cstdlib>
+#include <vector>
 
 unsigned int manhattanDist(class Item *, class Item*);
+
+class HashTable{
+    unsigned int bucketSize;
+    vector<class Item*> *buckets;
+    
+    public:
+    HashTable();
+    HashTable(unsigned int size);
+    ~HashTable();
+
+    //hashfunction operations
+    int insertItem(class Item*,unsigned int position);
+    int findItem(class Item*);
+};
 
 #endif
